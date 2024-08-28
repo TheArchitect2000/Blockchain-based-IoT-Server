76cf6503d9f0ff7ae3bbdd4b595836ca268a645b
HeaVeN <shawn@fidesinnova.io>
2024-08-28 15:24:01 +0330

    FrontEnd Admin

--- admin_web_app/Source_webapp/src/views/requests/components/RequestTable.tsx
+++ admin_web_app/Source_webapp/src/views/requests/components/RequestTable.tsx
@@ -61,7 +61,7 @@ const RequestTable: React.FC<UsersTableProps> = ({ setCount }) => {
                         item.published == true) &&
                     (!item.nodeServiceId ||
                         item.nodeServiceId == null ||
-                        item.nodeServiceId == undefined)
+                        item.nodeServiceId == undefined || item.nodeServiceId == "undefined")
             )
             setData(data.data)
             setFilteredData(data.data)
