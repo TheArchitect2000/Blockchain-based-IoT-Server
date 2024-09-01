11c894d93bb0665bcf1de65c16ac15de33552e07
root <shawn-dv>
2024-09-01 10:12:43 +0000

    BackEnd Update
    Devices list json file

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { UserPermissionService } from './modules/user/services/user-permission/user-permission.service';
 import { UserRoleService } from './modules/user/services/user-role/user-role.service';
@@ -7,21 +7,13 @@ import { UserService } from './modules/user/services/user/user.service';
 import { ErrorTypeEnum } from './modules/utility/enums/error-type.enum';
 import { GeneralException } from './modules/utility/exceptions/general.exception';
 import { JwtAuthGuard } from './modules/authentication/guard/jwt-auth.guard';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Controller('app')
 export class AppController {
   private result;
-  private baseUrl =
-    process.env.HOST_PROTOCOL +
-    process.env.HOST_NAME_OR_IP +
-    '/' +
-    process.env.HOST_SUB_DIRECTORY +
-    '/uploads/devices';
-  private deviceList = [
-    { url: `${this.baseUrl}/ecard.png`, title: 'E-Card' },
-    { url: `${this.baseUrl}/motionsensor.png`, title: 'Motion Sensor' },
-    { url: `${this.baseUrl}/MultiSensor.png`, title: 'Multi Sensor' },
-  ];
+  private deviceList: any[] = [];
 
   constructor(
     private readonly appService: AppService,
@@ -31,7 +23,71 @@ export class AppController {
   ) {
     setTimeout(() => {
       this.initializeApplication();
+      this.createDataFiles();
     }, 2000);
+  }
+
+  async createDataFiles() {
+    const dataFiles = [
+      {
+        fileName: 'devices.json',
+        content:
+          '[{ "url": "ecard.png", "title": "E-Card", "type": "E-CARD" }, { "url": "motionsensor.png", "title": "Motion Sensor", "type": "MULTI_SENSOR" }]',
+        filePath: path.join(__dirname, '..', 'src/data', 'devices.json'),
+      },
+      {
+        fileName: '',
+        content: null,
+        filePath: path.join(__dirname, '..', 'uploads/devices'), // Example of creating an empty folder
+      },
+    ];
+
+    for (const dataFile of dataFiles) {
+      const { filePath, content } = dataFile;
+      const directoryPath =
+        content === null ? filePath : path.dirname(filePath); // Determine directory path
+
+      try {
+        // Ensure the directory exists
+        await fs.promises.mkdir(directoryPath, { recursive: true });
+
+        if (content !== null) {
+          try {
+            // Check if the file already exists
+            await fs.promises.access(filePath, fs.constants.F_OK);
+            console.log(`File ${filePath} already exists. Skipping creation.`);
+          } catch (error) {
+            if (error.code === 'ENOENT') {
+              // File does not exist, so create it
+              await fs.promises.writeFile(filePath, content);
+              console.log(`File ${filePath} created successfully.`);
+            } else {
+              throw error; // Re-throw unexpected errors
+            }
+          }
+        } else {
+          console.log(`Folder ${directoryPath} created successfully.`);
+        }
+      } catch (error) {
+        console.error(`Error processing ${filePath}:`);
+      }
+    }
+
+    this.loadDeviceList();
+  }
+
+  async loadDeviceList() {
+    const filePath = path.join(__dirname, '..', 'src/data', 'devices.json');
+
+    try {
+      const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+      this.deviceList = JSON.parse(fileContent).map((device) => ({
+        ...device,
+        url: `${process.env.HOST_PROTOCOL}${process.env.HOST_NAME_OR_IP}/${process.env.HOST_SUB_DIRECTORY}/uploads/devices/${device.url}`,
+      }));
+    } catch (error) {
+      console.error('Error reading devices.json:');
+    }
   }
 
   async initializeApplication() {
@@ -43,7 +99,6 @@ export class AppController {
       .catch((error) => {
         let errorMessage =
           'Some errors occurred while inserting default permissions!';
-
         throw new GeneralException(
           ErrorTypeEnum.UNPROCESSABLE_ENTITY,
           errorMessage,
@@ -59,7 +114,6 @@ export class AppController {
         let errorMessage =
           'Some errors occurred while inserting default roles!';
         console.log(error);
-
         throw new GeneralException(
           ErrorTypeEnum.UNPROCESSABLE_ENTITY,
           errorMessage,
@@ -73,7 +127,7 @@ export class AppController {
   /* @UseGuards(JwtAuthGuard)
   @ApiBearerAuth() */
   @ApiOperation({
-    summary: 'get mobile them colors.',
+    summary: 'get mobile theme colors.',
     description: '',
   })
   async getThemeColors() {
@@ -90,7 +144,7 @@ export class AppController {
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @ApiOperation({
-    summary: 'get device.',
+    summary: 'get device list.',
     description: '',
   })
   async getDevices() {
