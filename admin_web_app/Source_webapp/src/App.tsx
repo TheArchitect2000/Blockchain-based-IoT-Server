601f264de10b8dc44c71c180fb7b6e69ad1ee44e
root <shawn-dv>
2025-01-31 18:24:55 +0000

    Fix

--- admin_web_app/Source_webapp/src/App.tsx
+++ admin_web_app/Source_webapp/src/App.tsx
@@ -9,6 +9,7 @@ import appConfig from '@/configs/app.config'
 import './locales'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const environment = process.env.NODE_ENV
 
@@ -30,13 +31,15 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                    <BrowserRouter>
-                        <Theme>
-                            <Layout />
-                        </Theme>
-                    </BrowserRouter>
-                </PersistGate>
+                <GoogleOAuthProvider clientId="990952057079-r35cambumvgrl8pqcvegi676gplmilq2.apps.googleusercontent.com">
+                    <PersistGate loading={null} persistor={persistor}>
+                        <BrowserRouter>
+                            <Theme>
+                                <Layout />
+                            </Theme>
+                        </BrowserRouter>
+                    </PersistGate>
+                </GoogleOAuthProvider>
             </Provider>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
