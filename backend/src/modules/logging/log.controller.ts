9d822781ef63e9b29a3d0bf7ccf91de225ab3d0c
neuralarchitects <thearchitecttraining@gmail.com>
2025-11-10 07:35:13 +0000

    remove userId in log-info

--- backend/src/modules/logging/log.controller.ts
+++ backend/src/modules/logging/log.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  Get,
-  HttpCode,
-  Inject,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, HttpCode, Inject, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '../authentication/guard/jwt-auth.guard';
@@ -26,10 +19,7 @@ export class LogController {
   @HttpCode(200)
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  async getLogs(@Request() request) {
-    return this.logService.getInternalLogs(
-      process.env.NODE_NAME,
-      request.user.userId,
-    );
+  async getLogs() {
+    return this.logService.getInternalLogs(process.env.NODE_NAME);
   }
 }
